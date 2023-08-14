6959d481dea7c76296880cf876b1b57e78e38dcb
Deepthireddy56 <bobbadeepthi86@gmail.com>
2023-08-14 09:52:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cdn_boot(request):
+    return render(request,'cdn_boot.html')
+def cdn_boot1(request):
+    return render(request,'cdn_boot1.html')
